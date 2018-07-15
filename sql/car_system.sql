1d7bce4cc37fd03bfdefa066efe6d332418e9ef9
Haozi0456 <468164865@qq.com>
2018-07-15 15:53:44 +0800

    初始导入

---
+++ sql/car_system.sql
@@ -0,0 +1,169 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本机
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : car_system
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2018-07-15 15:47:05
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for carts
+-- ----------------------------
+DROP TABLE IF EXISTS `carts`;
+CREATE TABLE `carts` (
+  `cartId` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `isCheck` int(11) NOT NULL DEFAULT '1' COMMENT '是否选中 0 未选中, 1选中',
+  `carNum` varchar(255) NOT NULL COMMENT '车牌号',
+  `make` varchar(255) NOT NULL COMMENT '汽车品牌',
+  PRIMARY KEY (`cartId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of carts
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for configs
+-- ----------------------------
+DROP TABLE IF EXISTS `configs`;
+CREATE TABLE `configs` (
+  `configId` int(11) NOT NULL AUTO_INCREMENT,
+  `itemName` varchar(255) DEFAULT '' COMMENT '字段名称	',
+  `itemCode` varchar(255) DEFAULT '' COMMENT '字段代码',
+  `itemValue` varchar(255) DEFAULT '' COMMENT '字段值',
+  PRIMARY KEY (`configId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of configs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `goodsId` int(11) NOT NULL AUTO_INCREMENT,
+  `goodsSn` varchar(255) DEFAULT '' COMMENT '商品编号',
+  `productNo` varchar(20) DEFAULT '' COMMENT '商品货号',
+  `goodsName` varchar(255) DEFAULT '' COMMENT '商品名称',
+  `goodsImg` varchar(255) DEFAULT '' COMMENT '商品图片',
+  `warnStock` int(11) DEFAULT '0' COMMENT '预警库存',
+  `goodsStock` int(11) DEFAULT '0' COMMENT '商品总库存',
+  `goodsUnit` varchar(255) DEFAULT '' COMMENT '单位',
+  `goodsTips` varchar(500) DEFAULT '' COMMENT '促销信息',
+  `isSale` int(11) DEFAULT '1' COMMENT '是否上架 0 不上架, 1 上架',
+  `isBest` int(11) DEFAULT '0' COMMENT '是否精品	0:否 1:是',
+  `isHot` int(11) DEFAULT '0' COMMENT '是否热销产品	0:否 1:是',
+  `isNew` int(11) DEFAULT '0' COMMENT '是否新品	0:否 1:是',
+  `isRecom` int(11) DEFAULT '0' COMMENT '是否推荐',
+  `goodsDesc` varchar(500) DEFAULT '' COMMENT '商品描述',
+  `goodsStatus` int(11) DEFAULT '0' COMMENT '商品状态	-1:违规 0:未审核 1:已审核',
+  `saleNum` int(11) DEFAULT '0' COMMENT '总销售量',
+  `saleTime` datetime DEFAULT NULL COMMENT '上架时间',
+  `visitNum` int(11) DEFAULT '0' COMMENT '访问数',
+  `commentNum` int(11) DEFAULT '0' COMMENT '评价数',
+  `isSpec` int(11) DEFAULT '0' COMMENT '是否有规格	0:没有 1:有',
+  `gallery` text COMMENT '商品相册',
+  `goodsSeoKeywords` varchar(255) DEFAULT '' COMMENT '商品SEO关键字',
+  `illegalRemarks` varchar(255) DEFAULT '' COMMENT '状态说明	一般用于说明拒绝原因',
+  `dataFlag` int(11) DEFAULT '1' COMMENT '删除标志	-1:删除 1:有效',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`goodsId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for images
+-- ----------------------------
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `imgId` int(11) NOT NULL AUTO_INCREMENT,
+  `fromType` int(11) DEFAULT '0' COMMENT '来自哪	0：商家/用户 1：平台管理员',
+  `dataId` int(11) DEFAULT '0' COMMENT '对象ID	该图片信息所在表的Id',
+  `imgPath` varchar(500) DEFAULT '' COMMENT '图片路径',
+  `imgSize` int(11) DEFAULT '0' COMMENT '图片大小	单位：字节',
+  `isUse` int(11) DEFAULT '1' COMMENT '是否有使用	1:是 0：否',
+  `createTime` datetime DEFAULT NULL,
+  `fromTable` varchar(255) DEFAULT '' COMMENT '来自哪张表	该记录所在表',
+  `ownId` int(11) DEFAULT '0' COMMENT '上传者Id',
+  `dataFlag` int(11) DEFAULT '1' COMMENT '删除标志	-1:删除 1:有效',
+  PRIMARY KEY (`imgId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of images
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for manager
+-- ----------------------------
+DROP TABLE IF EXISTS `manager`;
+CREATE TABLE `manager` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `dataFlag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of manager
+-- ----------------------------
+INSERT INTO `manager` VALUES ('1', 'admin', '12152205', '2018-07-13 16:56:27', '1');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `orderId` int(11) NOT NULL AUTO_INCREMENT,
+  `orderNo` varchar(255) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `money` decimal(10,2) DEFAULT '0.00' COMMENT '实际订单总金额	进行各种折扣之后的金额',
+  `payFrom` int(11) DEFAULT '0' COMMENT '支付来源	1:支付宝，2：微信',
+  `orderRemarks` varchar(255) DEFAULT '' COMMENT '订单备注',
+  `createTime` datetime DEFAULT NULL COMMENT '下单时间',
+  PRIMARY KEY (`orderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) DEFAULT '',
+  `name` varchar(50) DEFAULT '',
+  `gender` int(11) DEFAULT '1' COMMENT '性别, 1 - 男 0- 女',
+  `phone` varchar(255) DEFAULT '' COMMENT '手机号,登录账号',
+  `password` varchar(255) DEFAULT '' COMMENT '密码',
+  `avatarUrl` varchar(255) DEFAULT '' COMMENT '头像路径',
+  `nickName` varchar(255) DEFAULT '' COMMENT '昵称',
+  `createTime` datetime DEFAULT NULL,
+  `lastVisitTime` datetime DEFAULT NULL,
+  `userStatus` int(11) DEFAULT '1' COMMENT '账号状态	0:停用 1:启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
